ea428ccf1670e6e216f668647b3fd32dd0e8eaae
Shota Yamamota <1230124fw@gmail.com>
2017-06-12 13:01:18 +0900

    不必要な部分消去 index.ios.js,index.android.js

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './src/App'
 import { Provider } from 'react-redux'
 import configureStore from './src/store/configureStore'
